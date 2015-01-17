ce21c03375760e586405a1c23bc0360f3ac5fae0
Antonio J. García Lagar <aj@garcialagar.es>
2015-01-17 15:17:56 +0100

    Fix copyright statements

--- AjglSessionExpirationBundle.php
+++ AjglSessionExpirationBundle.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the AJ General Libraries Bundles
+ * This file is part of the AJGL packages
  *
- * Copyright (C) 2010-2014 Antonio J. García Lagar <aj@garcialagar.es>
+ * Copyright (C) Antonio J. García Lagar <aj@garcialagar.es>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,9 +11,9 @@
 
 namespace Ajgl\Bundle\SessionExpirationBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Ajgl\Bundle\SessionExpirationBundle\DependencyInjection\Compiler\AddSessionExpirationListenerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Antonio J. García Lagar <aj@garcialagar.es>
